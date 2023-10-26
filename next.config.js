adec011a067568b1e0e99f8a12b532800dbd7129
wallis <wallismark@gmail.com>
2023-10-26 22:16:57 +0200

    fixed the desplay hero image and images urls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // images: {
+    //     domains: ['images.unsplash.com'], // Add 'unsplash.com' to the list of allowed domains
+    //     // You can also include other domains if needed, separated by commas
+    //     // domains: ['unsplash.com', 'example.com'],
+    // },
+
+    // images: {
+    //     remotePatterns: [
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'images.unsplash.com',
+    //         port: '', // Leave it empty
+    //         pathname: '/*', // Use the wildcard to match any path
+    //       },
+    //     ],
+    //   },
+
     images: {
-        domains: ['images.unsplash.com'], // Add 'unsplash.com' to the list of allowed domains
-        // You can also include other domains if needed, separated by commas
-        // domains: ['unsplash.com', 'example.com'],
-    },
+        domains: ['images.unsplash.com', 'unsplash.com'], // Add 'unsplash.com' to allow Unsplash URLs
+      },
     experimental: {
         serverActions: true,
     },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/db/prisma'
 import Image from 'next/image'
 import ProductCard from '../components/ProductCard'
+import Link from 'next/link'
 
 export default async function Home() {
 
@@ -9,7 +10,28 @@ export default async function Home() {
   })
   return (
    <div>
-    <ProductCard product={products[1]}/>
+    <div className="hero rounded-xl bg-base-200">
+      <div className='hero-content flex-col lg:flex-row'>
+      <Image 
+      src={products[0].imageUrl}
+      alt={ products[0].name}
+      width={400}
+      height={800}
+      className='w-full max-w-sm rounded-lg shadow-2xl'
+      priority
+      />
+      <div>
+        <h1 className='text-5xl font-bold'>{products[0].name}</h1>
+        <p className='py-6'>{products[0].description}</p>
+        <Link 
+        href={"/products/" + products[0].id}
+        className="btn-primary btn">
+          Check it out
+        </Link>
+      </div>
+      </div>
+      
+    </div>
    </div>
   )
 }
