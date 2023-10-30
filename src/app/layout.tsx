9adddeac87e83c018ceb8d9359a8abd9051d046e
wallis <wallismark@gmail.com>
2023-10-30 14:33:11 +0200

    implemented the Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './Navbar/Navbar'
+import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <main className='p-4 max-w-7xl m-auto min-w-[300px]'>
       {children}
       </main>  
+      <Footer/>
       </body>
     </html>
   )
